36d66d8e51916f4a077411c1e84e14cc70a18282
Lucas <lbam@cin.ufpe.br>
2018-08-26 23:20:09 -0300

    Add matchmaking

--- index.js
+++ index.js
@@ -5,10 +5,8 @@ const app = express();
 const path = require('path');
 const server = http.Server(app);
 const io = socketIO(server);
-const Game = require('./lib/Game');
+const Lobby = require('./lib/Lobby');
 
-
-const FPS = 60;
 app.set('port', 5000);
 app.use(express.static(__dirname + '/public'));
 
@@ -22,38 +20,36 @@ server.listen(5000, function() {
   console.log('Starting server on port 5000');
 });
 
-let colors = ['red', 'blue'];
-let colorSelect = 0;
+// let colors = ['red', 'blue'];
+// let colorSelect = 0;
 
+let lobby = Lobby.create();
 // Add the WebSocket handlers
-let game = Game.create();
 io.on('connection', function(socket) {
     socket.on('player-join', function() {
-        game.addNewPlayer(socket, colors[colorSelect]);
-        if (colorSelect == 0) colorSelect = 1;
-        else colorSelect = 0;
+        lobby.matchmake(socket);
+        // game.addNewPlayer(socket, colors[colorSelect]);
+        // if (colorSelect == 0) colorSelect = 1;
+        // else colorSelect = 0;
     });
 
     socket.on('action', function(data) {
-        let player = game.players.get(socket.id);
-        if (player) {
-            player.updateByInput(data);
+        let game = lobby.searchRunningGame(socket.id);
+        if (game) {
+            let player = game.players.get(socket.id);
+            if (player) player.updateByInput(data);
         }
     });
 
     socket.on('disconnect', function() {
-        let player = game.players.get(socket.id);
-        if (player) {
-            if (player.color == 'red') {
-                colorSelect = 0;
-            } else {
-                colorSelect = 1;
-            }
-        }
-        game.removePlayer(socket.id);
+        lobby.removePlayer(socket.id);
+        // if (player) {
+        //     if (player.color == 'red') {
+        //         colorSelect = 0;
+        //     } else {
+        //         colorSelect = 1;
+        //     }
+        // }
     });
 });
 
-setInterval(function() {
-    game.updateState();
-}, 1000 / FPS);
